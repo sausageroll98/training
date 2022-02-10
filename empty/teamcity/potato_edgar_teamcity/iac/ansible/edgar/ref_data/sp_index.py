b26b005313fd62986c406a9f9555576a2a1d5374
sausageroll98 <96574863+sausageroll98@users.noreply.github.com>
2022-02-10 14:59:46 +0000

    commit

---
+++ empty/teamcity/potato_edgar_teamcity/iac/ansible/edgar/ref_data/sp_index.py
@@ -0,0 +1,41 @@
+'''
+Author: Albert Tran
+Created: 2020-08-08
+
+Module to get S&P500 reference data. Data is a snapshot taken from:
+https://en.wikipedia.org/wiki/List_of_S%26P_500_companies
+'''
+# pylint: disable=E0401
+# %%
+# ------------------------------------------------------------------------------
+# Imports
+#-------------------------------------------------------------------------------
+import os
+import pandas as pd
+import pkg_resources
+
+
+# %%
+# ------------------------------------------------------------------------------
+# File Path
+#-------------------------------------------------------------------------------
+# Setting up the file path
+DATA_PATH = pkg_resources.resource_filename(__name__, '../ref_data/csv/')
+file_path_sp100 = os.path.join(DATA_PATH,'sp100_companies.csv')
+file_path_sp500 = os.path.join(DATA_PATH,'sp500_companies.csv')
+
+
+# %%
+# ------------------------------------------------------------------------------
+# Data read
+#-------------------------------------------------------------------------------
+def get_sp500():
+    '''GETS SP500 DATA'''
+    return pd.read_csv(file_path_sp500)
+
+
+def get_sp100():
+    '''GETS SP100 DATA'''
+    df_sp100 = pd.read_csv(file_path_sp100)
+    df_sp500 = get_sp500()
+    return df_sp500[df_sp500['Symbol'].isin(df_sp100['Symbol'])]
